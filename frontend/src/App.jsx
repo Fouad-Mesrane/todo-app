2ccadcc81986dc55281186fe93d20d3abb446822
fouad mesrane <fouadmesrane@gmail.com>
2025-02-17 11:40:57 -0700

    navbar ui

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,15 +3,21 @@ import HomePage from "./pages/HomePage";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import NotFound from "./pages/NotFound";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/auth/register" element={<Register/>} />
-      <Route path="/auth/login" element={<Login/>} />
-      <Route path="*" element = {<NotFound/>}/>
-    </Routes>
+    <div className="text-xs md:text-lg md:py-16 md:px-36">
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/auth/register" element={<Register />} />
+        <Route path="/auth/login" element={<Login />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+      <Footer/>
+    </div>
   );
 }
 
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,9 +1,54 @@
-import React from 'react'
+import React, { useState,useEffect } from "react";
+import { Link, NavLink } from "react-router-dom";
 
 const Navbar = () => {
-  return (
-    <nav>Navbar</nav>
-  )
-}
+  const [menuOpen, setMenuOpen] = useState(false);
 
-export default Navbar
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth >= 768) {
+        setMenuOpen(false);
+      }
+    };
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  return (
+    <nav className="md:flex justify-between items-center p-4  relative">
+      {/* Logo + Mobile Menu Button */}
+      <div className="flex gap-4 items-center">
+        <Link className="flex gap-4 items-center" to={"/"}>
+        <img className="w-6" src="/assets/navbar/icon.png" alt="icon" />
+          <p className="font-bold text-primary">Todo Daily</p>
+        </Link>
+
+        {/* Mobile Menu Button */}
+        <button
+          onClick={() => setMenuOpen(!menuOpen)}
+          className="md:hidden text-primary text-2xl ml-auto"
+        >
+          ☰
+        </button>
+      </div>
+
+      {/* Navigation Links  */}
+      <ul
+        className={`md:flex gap-4 ${
+          menuOpen ? "absolute top-12 left-0 w-full bg-white p-4 shadow-md" : "hidden md:flex"
+        }`}
+      >
+        <NavLink to={"/auth/login"} className="block py-2 md:py-0">
+          Login
+        </NavLink>
+        <NavLink to={"/auth/register"} className="block py-2 md:py-0">
+          Signup
+        </NavLink>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;

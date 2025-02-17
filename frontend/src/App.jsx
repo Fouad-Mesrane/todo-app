1d691a228c6fd4896381dde404e42326909694eb
fouad mesrane <fouadmesrane@gmail.com>
2025-02-17 09:43:08 -0700

    setup react router on frontend and aadded pages and routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,18 @@
-import HomePage from "./pages/HomePage"
-
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import NotFound from "./pages/NotFound";
 
 function App() {
-
-
   return (
-    <>
-    <HomePage/>
-    </>
-  )
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/auth/register" element={<Register/>} />
+      <Route path="/auth/login" element={<Login/>} />
+      <Route path="*" element = {<NotFound/>}/>
+    </Routes>
+  );
 }
 
-export default App
+export default App;
